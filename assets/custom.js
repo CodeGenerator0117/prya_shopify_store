45674b66a1a36056407f331887e1b47dad4935be
CodeGenerator0117 <akirasuzuki0117@gmail.com>
2024-06-25 04:56:29 -0400

    update

--- assets/custom.js
+++ assets/custom.js
@@ -742,31 +742,17 @@ $(".product-form__submit_builder").click(function (e) {
         contentType: "application/json; charset=utf-8",
         data: JSON.stringify(formData),
         success: function (res) {
-            $.ajax({
-                type: "GET",
-                url: `${routes.cart_url}?view=rendercart`,
-                success: function (htm) {
-                    $("#CartDrawer-CartItems").html(htm),
-                        that.find("span").show(),
-                        that.find(".loading-overlay__spinner").addClass("hidden"),
-                        $(".modal").removeClass("is-visible"),
-                        that.hasClass("propage") && $("cart-drawer").addClass("animate active")
-                },
-                error: function (err) {
-                    console.log(err),
-                        that.find("span").show(),
-                        that.find(".loading-overlay__spinner").addClass("hidden")
+            jQuery.getJSON("/cart.js", function (cart) {
+                if ($(".cart-count-bubble")[0])
+                    $(".cart-count-bubble span").html(cart.item_count);
+                else {
+                    var dt = '<div class="cart-count-bubble"><span aria-hidden="true">' + cart.item_count + '</span><span class="visually-hidden">' + cart.item_count + " item</span> </div>";
+                    $("#cart-icon-bubble").append(dt)
                 }
-            }),
-                jQuery.getJSON("/cart.js", function (cart) {
-                    if ($(".cart-count-bubble")[0])
-                        $(".cart-count-bubble span").html(cart.item_count);
-                    else {
-                        var dt = '<div class="cart-count-bubble"><span aria-hidden="true">' + cart.item_count + '</span><span class="visually-hidden">' + cart.item_count + " item</span> </div>";
-                        $("#cart-icon-bubble").append(dt)
-                    }
-                    cartLink.click()
-                })
+                cartLink.click()
+            })
+            that.find("span").show(),
+            that.find(".loading-overlay__spinner").addClass("hidden")
         },
         error: function (err) {
             console.log(err),
